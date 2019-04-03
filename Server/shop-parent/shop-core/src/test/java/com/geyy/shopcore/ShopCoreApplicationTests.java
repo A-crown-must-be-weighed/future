668441098e6cafb8c633978dff69eb1fcadbf3ca
geyaowei <31578892+geyaowei@users.noreply.github.com>
2019-04-03 21:09:31 +0800

    后台架构

---
+++ Server/shop-parent/shop-core/src/test/java/com/geyy/shopcore/ShopCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.geyy.shopcore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopCoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
